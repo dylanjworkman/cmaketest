97087dc91945592a9b7d254caec5bf1b3cd14d8b
Dylan Workman <56436950+dylanjworkman@users.noreply.github.com>
2020-12-03 16:42:13 +0000

    main c++ file created

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv) 
+{
+	cout << "This is a test for CMake game engine build!" << endl;
+
+	exit(0);
+}
